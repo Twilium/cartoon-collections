fa75e213ab46a3eafaa4e76a6fff8ca542d18e61
Emmanuel <rivera128@live.com>
2020-10-31 00:44:30 -0500

    commit tree 685e7c4aa5f9dbc334b49bde1bb37d776dfae76a

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,23 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarf_array)
+  dwarf_array.each.with_index(1) do |dwarf, index|
+    puts "#{index}. #{dwarf}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_array)
+  collection = []
+  planeteer_array.map do |planeteer|
+    collection << "#{planeteer.capitalize}!"
+  end
+  collection
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(call_array)
+  call_array.any? { |word| word.length > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(string_array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  string_array.find { |word| cheese_types.include?(word)}
 end
